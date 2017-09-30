7737f6c62b1c255baa1a7994ce0889d5106c786f
sami79031 <sami79031@gmail.com>
2017-09-30 13:12:44 +0300

    Initial Commit

---
+++ SashidoParseIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SashidoParseIntegration
+//
+//  Created by Sami Ali on 9/30/17.
+//  Copyright © 2017 Sashido.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
